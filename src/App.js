34b195632e8acff02db3b3edf0fb09a487607701
Ratnadasdeveloper <104377250+Ratnadasdeveloper@users.noreply.github.com>
2022-05-29 15:28:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import './App.css';
+import Hello from './components/Hello.jsx';
+
+function App() {
+  return (
+    <div className='App'>
+       
+       <Hello/>
+
+    </div>
+  );
+}
+
+
+
+export default App;
